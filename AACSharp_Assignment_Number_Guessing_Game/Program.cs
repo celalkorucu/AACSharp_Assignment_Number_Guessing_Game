d0ae6af4640423295ddde4e083948106b3cdad1d
celalkorucu <21010310079@ogrenci.bartin.edu.tr>
2025-03-17 11:43:14 +0300

    Add project files.

---
+++ AACSharp_Assignment_Number_Guessing_Game/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Random rnd = new Random();
+int randomNumber = rnd.Next(1, 100);
+
+Console.WriteLine(randomNumber);
+
+int count = 0;
+while (true)
+{
+    count++;
+    Console.WriteLine("Aklımdan tuttuğum sayıyı tahmin et bakalım : ");
+    int guessNumber = int.Parse(Console.ReadLine());
+
+    if(guessNumber>0 && guessNumber < 100)
+    {
+        if (randomNumber == guessNumber)
+        {
+            Console.WriteLine("DOĞRU TAHMİNNN.");
+            Console.WriteLine("Aklımdan tuttuğum sayı " + randomNumber + " idi.");
+            Console.WriteLine(count + " Tahminde doğru sayıyı bulması başardın BRAVO");
+            break;
+        }
+        else if (randomNumber > guessNumber)
+        {
+            Console.WriteLine("Daha yüksek bir sayı tahmin etmen gerekiyor");
+        }
+        else if (randomNumber < guessNumber)
+        {
+            Console.WriteLine("Daha düşük bir sayı tahmin etmen gerekiyor");
+        }
+    }
+    else
+    {
+        Console.WriteLine("0 ile 100 arasında bir sayı girmelisin");
+    }
+
+   
+}
+
+Console.WriteLine("GAME OVER");
+
